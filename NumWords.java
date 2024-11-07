a62490dce13b7815b9c12aa80aa94fba7bb14cca
udi hochberg <udi.hochberg@post.runi.ac.il>
2024-11-07 19:47:35 +0200

    init commit3

--- NumWords.java
+++ NumWords.java
@@ -5,6 +5,6 @@ public class NumWords {
 	   int hundreds = num / 100;
 	   int tens = (num / 10) % 10;
 	   int ones = num % 10;
-	   System.out.println(hundreds+" hundreds, "+tens+" tens and "+ones+" ones.");
+	   System.out.println(hundreds+" hundreds, "+tens+" tens, and "+ones+" ones.");
 	}
 }
