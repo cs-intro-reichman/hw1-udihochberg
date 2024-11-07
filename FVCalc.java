442d0e261bd932b68fdc39315b4626eeff0c5041
udi hochberg <udi.hochberg@post.runi.ac.il>
2024-11-07 14:58:03 +0200

    init commit3

--- FVCalc.java
+++ FVCalc.java
@@ -2,7 +2,7 @@
 public class FVCalc {
 	public static void main(String[] args){
 		double currentValue = Double.parseDouble(args[0]);
-		int rate = Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
 		int countYears = 0;
 		double newValue = currentValue;
@@ -11,7 +11,7 @@ public class FVCalc {
 			newValue = newValue + (newValue * rate / 100);
 			countYears++;
 		}
-		System.out.println("After "+years+" years, a $"+(int)currentValue+" saved at "+rate+".0% will yield $"+(int)newValue); 
+		System.out.println("After "+years+" years, a $"+(int)currentValue+" saved at "+rate+" will yield $"+(int)newValue); 
 
 	}
 }
