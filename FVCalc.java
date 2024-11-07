810c6e30b134e51978bf6f54d92e7db4367821c7
udi hochberg <udi.hochberg@post.runi.ac.il>
2024-11-07 14:30:45 +0200

    init commit1

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,17 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code dsjkfhdjks
+		double currentValue = Double.parseDouble(args[0]);
+		int rate = Integer.parseInt(args[1]);
+		int years = Integer.parseInt(args[2]);
+		int countYears = 0;
+		double newValue = currentValue;
+		while (countYears < years)
+		{
+			newValue = newValue + (newValue * rate / 100);
+			countYears++;
+		}
+		System.out.println("After "+years+" years, $"+(int)currentValue+" saved at "+rate+".0% will yield $"+(int)newValue); 
+
 	}
 }
