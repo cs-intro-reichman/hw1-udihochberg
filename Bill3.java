86d45621c4e2132e427f0ae87cfe9feb72bd65f4
udi hochberg <udi.hochberg@post.runi.ac.il>
2024-11-07 12:57:25 +0200

    init commit2

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,11 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+	public static void main(String[] args) {		
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int totalDinner = Integer.parseInt(args[3]);
+		double newSum = Math.ceil(totalDinner / 3.0);
+		System.out.println("Dear "+name1+", "+name2+"and "+name3+": pay "+newSum+" Shekels each." );      
 	}
 }
